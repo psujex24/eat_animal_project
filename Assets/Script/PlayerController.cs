f81e94f0a5b9c2e96ab8fe4c8560c998899a7c8b
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-03 22:18:56 +0200

    1 lesson

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
     }
