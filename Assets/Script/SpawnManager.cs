b2544ac484e74382d02648f1419b83c3ee941dd1
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-06 00:27:34 +0200

    lesson 3.2

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPreFabs;
-    public int animalIndex;
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +18,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPreFabs[animalIndex], new Vector3(0, 0, 20), animalPreFabs[animalIndex].transform.rotation);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
