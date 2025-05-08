743435c9f1b64baaa987f5a6d33ddd560b506af4
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-08 23:46:28 +0200

    fix bugs on engine and some on coce

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,7 +11,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = Random.Range(3, 5);
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +20,16 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
-
+    float GetRandomInterval()
+    {
+        return Random.Range(3f, 5f);
+    }
 }
