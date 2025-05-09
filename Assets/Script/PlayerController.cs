dd4cf4a69e12bb096fa2c0632aa8cb0af2c3ffa3
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-09 23:33:31 +0200

    add to core project movment horizontal and vertical

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
+    private float horizontalInput;
+    private float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRange = 15.0f;
 
 
     public GameObject projectilePrefab;
@@ -14,9 +16,8 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    {   //poruszanmie sie po osi x z "niewidzialna sciana"
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -25,9 +26,22 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        //poruszanie si epo osi z z "niewidzialna sciana"
+        if (transform.position.z < 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        //wyłapywanmie klikniecia przyciskow
+        verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
+        //tempo ruchu 
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+        //spawnowanie obiektow (steków)
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
