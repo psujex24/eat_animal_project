8e889da43f0d31235235507c09c11ebd88050766
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-09 23:18:13 +0200

    add spam spawning dogs

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    public float lastSwapTime;
     public GameObject dogPrefab;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+
+        if (Time.time - lastSwapTime > 1.0f)
+        { //enough time has elapsed
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+                lastSwapTime = Time.time;
+            }
         }
     }
 }
+
+
+
+
