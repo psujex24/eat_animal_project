5f97946459f38fea0390d35b775108ffc79b2a70
Psujex24 <70110209+psujex24@users.noreply.github.com>
2025-05-12 22:36:54 +0200

    ni mam pojecia poddaje sie

--- Assets/Script/DetectCollisions.cs
+++ Assets/Script/DetectCollisions.cs
@@ -5,29 +5,31 @@ public class DetectCollisions : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
-        // Sprawdzamy, czy kolizja jest z jedzeniem (steakiem)
+        Debug.Log("Zderzenie z: " + other.gameObject.name + ", tag: " + other.tag);
+
         if (other.CompareTag("Food_Steak_01"))
         {
-            Debug.Log("Game Over! Pizze zniszczone!");
-            Destroy(gameObject);  // Zniszczenie zwierzęcia
-            Destroy(other.gameObject);  // Zniszczenie steak (jedzenie)
+            Debug.Log("Kolizja z jedzeniem – niszczę oba");
+            Destroy(gameObject);             // Zniszcz zwierzę
+            Destroy(other.gameObject);      // Zniszcz jedzenie
         }
-        // Sprawdzamy, czy kolizja jest z gracza (jeśli chcesz mieć specjalną logikę dla gracza)
-        else if (other.CompareTag("Player"))
+        else if (
+            other.CompareTag("Animal_Stag_02") ||
+            other.CompareTag("Animal_Fox_02") ||
+            other.CompareTag("Animal_Doe_02") ||
+            other.CompareTag("Player")
+        )
         {
-            Debug.Log("Game Over! Gracz zniszczony!");
-            Destroy(gameObject);  // Zniszczenie zwierzęcia
+            // NIC nie rób: ani zwierzę, ani gracz nie są niszczeni
+            Debug.Log("Kolizja z graczem lub innym zwierzęciem – brak działania");
         }
-        // Sprawdzamy kolizję z innymi zwierzętami - nie robimy nic w tym przypadku
-        else if (other.CompareTag("Animal_Stag_02") || other.CompareTag("Animal_Fox_02") || other.CompareTag("Animal_Doe_02"))
-        {
-            // Zwierzęta nie niszczą siebie nawzajem, więc nic się nie dzieje
-        }
-        // W przeciwnym razie, niszczymy obiekt, jeśli to nie jest zwierzę ani steak
         else
         {
+            // Jeśli kolizja z czymś innym (np. ścianą), zniszcz zwierzę
+            Debug.Log("Kolizja z czymś innym – niszczę zwierzę");
             Destroy(gameObject);
         }
     }
-
 }
+
+
